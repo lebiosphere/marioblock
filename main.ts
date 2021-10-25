2eab4a319a232b4ea1729e5b7dcb9017f4cce62d
lebiosphere <lebiosphere@gmail.com>
2021-10-25 09:06:45 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,16 +7,19 @@ let bascule = 0
 radio.setGroup(1)
 let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 strip.showColor(neopixel.hsl(0, 100, 2))
+basic.showIcon(IconNames.No)
 bascule = 0
 basic.forever(function () {
     if (bascule == 1 || input.buttonIsPressed(Button.A)) {
         bascule = 1
+        basic.showIcon(IconNames.Heart)
         strip.showColor(neopixel.hsl(255, 100, 100))
         while (bascule == 1) {
             if (input.isGesture(Gesture.Shake)) {
                 radio.sendString("step2")
                 strip.showColor(neopixel.hsl(0, 100, 2))
                 bascule = 0
+                basic.showIcon(IconNames.No)
             }
         }
     }
